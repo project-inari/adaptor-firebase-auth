8722b80abcde8383d8f7e1732d35b08f8d45cfac
Thanatpong [Win] Songjitsomboon <winthanatpong@gmail.com>
2025-01-09 14:00:11 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# adaptor-firebase-auth
+
+GO codebase repository for Inari backend system microservice `adaptor-firebase-auth`.
+
+[Kustomize Repo](URL)<br>
+[ArgoCD](URL)<br>
+[Confluence Docs](URL)<br>
+
+## Development
+Prepare the development environment:
+```bash
+make prepare
+```
+
+Run in local development environment:
+```bash
+make dev.up
+```
+
+Run in alpha development environment:
+```bash
+make dev.up.alpha
+```
---
+++ main.go
@@ -0,0 +1,49 @@
+// Package main is the main entry point for adaptor-firebase-auth service
+package main
+
+import (
+	"log"
+	"log/slog"
+	"os"
+	"runtime"
+	"time"
+
+	"gitlab.com/greyxor/slogor"
+
+	"github.com/project-inari/adaptor-firebase-auth/config"
+	"github.com/project-inari/adaptor-firebase-auth/di"
+	_ "github.com/project-inari/adaptor-firebase-auth/docs"
+)
+
+// @title API Endpoints for adaptor-firebase-auth
+// @version 1.0
+
+func init() {
+	runtime.GOMAXPROCS(1)
+
+	location, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		log.Panicf("failed to set timezone: %v", err)
+	}
+	time.Local = location
+}
+
+func main() {
+	// Initialize logger
+	env := os.Getenv("APP_ENV_STAGE")
+	var logger *slog.Logger
+	if env == "" || env == "LOCAL" {
+		logger = slog.New(slogor.NewHandler(os.Stdout, slogor.SetTimeFormat(time.Stamp), slogor.ShowSource()))
+	} else {
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+		}))
+	}
+	slog.SetDefault(logger)
+
+	// Initiaize config
+	cfg := config.New(env)
+
+	// Initialize dependency injection
+	di.New(cfg)
+}
