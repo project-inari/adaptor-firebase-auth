cc5ec5065f937020b90a3b4bc03f26be97a5cc7b
Thanatpong [Win] Songjitsomboon <winthanatpong@gmail.com>
2025-01-13 08:55:02 +0700

    feat: add logtracing middleware

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"log"
 	"log/slog"
+	"os"
 	"sync"
 
 	"github.com/caarlos0/env/v11"
@@ -15,8 +16,9 @@ var once sync.Once
 var config *Config
 
 // New loads the configuration from the .env file
-func New(e string) *Config {
+func New() *Config {
 	once.Do(func() {
+		e := os.Getenv("APP_ENV_STAGE")
 		if e == "" || e == "LOCAL" {
 			if err := godotenv.Load(".env.generated"); err != nil {
 				slog.Warn("[config.New] unable to load .env.generated file", slog.Any("error", err))
@@ -37,6 +39,7 @@ func New(e string) *Config {
 // Config represents the configuration of the server
 type Config struct {
 	AppConfig          AppConfig
+	LogConfig          LogConfig
 	SentryConfig       SentryConfig
 	FirebaseAuthConfig FirebaseAuthConfig
 }
@@ -46,6 +49,12 @@ type AppConfig struct {
 	Name     string `env:"APP_NAME,notEmpty"`
 	Port     string `env:"APP_PORT,notEmpty"`
 	EnvStage string `env:"APP_ENV_STAGE,notEmpty"`
+}
+
+// LogConfig represents the configuration of the logger
+type LogConfig struct {
+	Level             string `env:"LOG_LEVEL,notEmpty"`
+	MaskSensitiveData bool   `env:"LOG_MASK_SENSITIVE_DATA,notEmpty"`
 }
 
 // SentryConfig represents the configuration of Sentry.io
