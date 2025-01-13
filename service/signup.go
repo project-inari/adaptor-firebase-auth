647d853964b637a1f7604b7b600709b29da7a83f
Thanatpong [Win] Songjitsomboon <winthanatpong@gmail.com>
2025-01-13 10:06:52 +0700

    chore: add token to log masking

--- service/signup.go
+++ service/signup.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"log/slog"
 
 	"github.com/project-inari/adaptor-firebase-auth/dto"
 )
@@ -11,11 +10,8 @@ import (
 func (s *service) SignUp(ctx context.Context, req dto.SignUpReq, header dto.SignUpReqHeader) (*dto.SignUpRes, error) {
 	res, err := s.firebaseAuthRepository.SignUp(ctx, req, header)
 	if err != nil {
-		slog.Error("error - [SignUp] unable to sign up", slog.Any("error", err))
 		return &dto.SignUpRes{}, err
 	}
-
-	slog.Info("[firebaseAuthRepository.SignUp] user created successfully", slog.Any("username", req.Username))
 
 	return &dto.SignUpRes{
 		Token: res,
--- service/verify_token.go
+++ service/verify_token.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"log/slog"
 
 	"github.com/project-inari/adaptor-firebase-auth/dto"
 )
@@ -10,11 +9,8 @@ import (
 func (s *service) VerifyToken(ctx context.Context, req dto.VerifyTokenReq) *dto.VerifyTokenRes {
 	res, err := s.firebaseAuthRepository.VerifyToken(ctx, req.Token)
 	if err != nil {
-		slog.Error("error - [VerifyToken] unable to verify token", slog.Any("error", err))
 		return &dto.VerifyTokenRes{Success: false}
 	}
-
-	slog.Info("[firebaseAuthRepository.VerifyToken] token verified successfully", slog.Any("username", res.Username))
 
 	return &dto.VerifyTokenRes{
 		Username: res.Username,
